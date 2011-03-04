0c0dab868cdcb87e99ae5937c561e3f5ed05f85d
Mykola Rybak <mykola.rybak@gmail.com>
2011-03-04 13:43:56 +0200

    Рефакторинг класса JumpCounter

--- src/ru/socionicasys/analyst/JumpCounter.java
+++ src/ru/socionicasys/analyst/JumpCounter.java
@@ -3,38 +3,38 @@ package ru.socionicasys.analyst;
 import java.util.HashMap;
 
 public class JumpCounter {
-	private final HashMap<String, HashMap<String, Integer>> jumpTable;
+	private class JumpPair {
+		private final String from;
+		private final String to;
+
+		public JumpPair(final String from, final String to) {
+			this.from = from;
+			this.to = to;
+		}
+	}
+
+	private final HashMap<JumpPair, Integer> jumpTable;
 
 	public JumpCounter() {
-		jumpTable = new HashMap<String, HashMap<String, Integer>>();
+		jumpTable = new HashMap<JumpPair, Integer>();
 	}
 
 	public void addJump(String to, String from) {
-		HashMap<String, Integer> t;
-		if (!jumpTable.containsKey(to)) {
-			t = new HashMap<String, Integer>();
+		JumpPair p = new JumpPair(from, to);
+		if (jumpTable.containsKey(p)) {
+			jumpTable.put(p, jumpTable.remove(p) + 1);
 		} else {
-			t = jumpTable.get(to);
+			jumpTable.put(p, 1);
 		}
-		if (t.containsKey(from)) {
-			t.put(from, t.remove(from) + 1);
-		} else {
-			t.put(from, 1);
-		}
-		jumpTable.put(to, t);
 	}
 
 	public int getJumpCount(String to, String from) {
-		HashMap<String, Integer> t;
-		if (jumpTable.containsKey(to)) {
-			t = jumpTable.get(to);
+		JumpPair p = new JumpPair(from, to);
+		if (jumpTable.containsKey(p)) {
+			return jumpTable.get(p);
 		} else {
 			return 0;
 		}
-		if (!t.containsKey(from)) {
-			return 0;
-		}
-		return t.get(from);
 	}
 
 	public boolean isEmpty() {
