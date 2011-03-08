088700097ad8d654561ad70897c852c3d93305f4
Mykola Rybak <mykola.rybak@gmail.com>
2011-03-08 18:08:28 +0200

    Логи в классе Analyst

--- src/ru/socionicasys/analyst/Analyst.java
+++ src/ru/socionicasys/analyst/Analyst.java
@@ -1,9 +1,14 @@
 package ru.socionicasys.analyst;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.swing.*;
 
 public class Analyst {
+	private static final Logger logger = LoggerFactory.getLogger(Analyst.class);
+
 	public static void main(String[] args) {
+		logger.trace("> main()");
 		final String startupFilename;
 		if (args != null && args.length > 0) {
 			startupFilename = args[0];
@@ -17,6 +22,7 @@ public class Analyst {
 				createAndShowGUI(startupFilename);
 			}
 		});
+		logger.trace("< main()");
 	}
 
 	/**
@@ -28,11 +34,13 @@ public class Analyst {
 	 */
 	private static void createAndShowGUI(String startupFilename) {
 		//Create and set up the window.
+		logger.trace("> createAndShowGUI(), startupFilename={}", startupFilename);
 		final AnalystWindow frame = new AnalystWindow(startupFilename);
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
 		//Display the window.
 		frame.pack();
 		frame.setVisible(true);
+		logger.trace("< createAndShowGUI()");
 	}
 }
