cd4dea5dc41891de50ac1f186da1db2c3091dc27
Mykola Rybak <mykola.rybak@gmail.com>
2011-10-10 23:10:51 +0300

    Расширил объем логов в DocumentTransferHandler.

--- src/main/java/ru/socionicasys/analyst/DocumentTransferHandler.java
+++ src/main/java/ru/socionicasys/analyst/DocumentTransferHandler.java
@@ -27,41 +27,49 @@ public class DocumentTransferHandler extends TransferHandler {
 	 * @param parentTransferHandler родительский {@code TransferHandler}.
 	 */
 	public DocumentTransferHandler(TransferHandler parentTransferHandler) {
-		logger.debug("DocumentTransferHandler(): constructing with parent={}", parentTransferHandler);
+		logger.trace("DocumentTransferHandler({}): entering", parentTransferHandler);
 		this.parentTransferHandler = parentTransferHandler;
+		logger.trace("DocumentTransferHandler({}): leaving", parentTransferHandler);
 	}
 
 	@Override
 	public boolean canImport(TransferSupport support) {
-		logger.debug("canImport(): called with support={}", support);
+		logger.trace("canImport({}): entering", support);
 		boolean canImport = support.isDataFlavorSupported(ADocumentFragment.getNativeFlavor()) ||
 				parentTransferHandler != null && parentTransferHandler.canImport(support);
-		logger.debug("canImport(): result = {}", canImport);
+		logger.trace("canImport({}): leaving, result = {}", support, canImport);
 		return canImport;
 	}
 
 	@Override
 	public boolean importData(TransferSupport support) {
-		logger.debug("importData(): called with support={}", support);
+		logger.trace("importData({}): entering", support);
 		if (!canImport(support)) {
+			logger.debug("Cannot import data");
+			logger.trace("importData({}): leaving, result = false", support);
 			return false;
 		}
 
 		DataFlavor nativeFlavor = ADocumentFragment.getNativeFlavor();
 		if (!support.isDataFlavorSupported(nativeFlavor)) {
-			logger.debug("importData(): ADocumentFragment flavor not supported, delegating to parent");
-			return parentTransferHandler != null && parentTransferHandler.importData(support);
+			logger.debug("ADocumentFragment flavor not supported, delegating to parent");
+			boolean parentCanImport = parentTransferHandler != null && parentTransferHandler.importData(support);
+			logger.trace("importData({}): leaving, result = {}", support, parentCanImport);
+			return parentCanImport;
 		}
 
-		logger.debug("importData(): inserting ADocumentFragment");
+		logger.debug("Inserting ADocumentFragment");
 		ADocumentFragment fragment;
 		try {
 			fragment = (ADocumentFragment) support.getTransferable().getTransferData(nativeFlavor);
+			logger.debug("Obtained document fragment {}", fragment);
 		} catch (UnsupportedFlavorException e) {
 			logger.error("Unexpectedly unsupported native data flavour", e);
+			logger.trace("importData({}): leaving, result = false", support);
 			return false;
 		} catch (IOException e) {
 			logger.error("I/O error while making data transfer");
+			logger.trace("importData({}): leaving, result = false", support);
 			return false;
 		}
 
@@ -79,7 +87,9 @@ public class DocumentTransferHandler extends TransferHandler {
 			insertPosition = textPane.getCaretPosition();
 		}
 
+		logger.debug("Inserting document fragment at position {} (isDrop() = {})", insertPosition, support.isDrop());
 		document.pasteADocFragment(insertPosition, fragment);
+		logger.trace("importData({}): leaving, result = true", support);
 		return true;
 	}
 
@@ -91,28 +101,30 @@ public class DocumentTransferHandler extends TransferHandler {
 	@Override
 	@SuppressWarnings("ReturnOfNull")
 	protected Transferable createTransferable(JComponent c) {
-		logger.debug("createTransferable(): called with c={}", c);
+		logger.trace("createTransferable(): entering");
 		assert c instanceof TextPane : "DocumentTransferHandler is only usable with TextPane instance";
 		TextPane textPane = (TextPane) c;
 
 		int selectionStart = textPane.getSelectionStart();
 		int selectionEnd = textPane.getSelectionEnd();
 		if (selectionStart == selectionEnd) {
-			logger.debug("createTransferable(): empty selection, no fragment created");
+			logger.debug("Empty selection, no fragment created");
 			return null;
 		}
 
-		logger.debug("createTransferable(): creating new ADocumentFragment");
+		logger.debug("Creating new ADocumentFragment");
 		ADocument document = textPane.getDocument();
-		return document.getADocFragment(selectionStart, selectionEnd - selectionStart);
+		ADocumentFragment documentFragment = document.getADocFragment(selectionStart, selectionEnd - selectionStart);
+		logger.trace("createTransferable(): leaving");
+		return documentFragment;
 	}
 
 	@Override
 	protected void exportDone(JComponent source, Transferable data, int action) {
-		logger.debug("exportDone(): called with source={}, data={}, action={}",
-				new Object[]{source, data, action});
+		logger.trace("exportDone(source, {}, {}): entering", data, action);
 		if (action != MOVE) {
 			logger.debug("exportDone(): nothing to be done for this action");
+			logger.trace("exportDone(source, {}, {}): leaving", data, action);
 			return;
 		}
 
@@ -124,11 +136,14 @@ public class DocumentTransferHandler extends TransferHandler {
 
 		ADocument document = textPane.getDocument();
 		try {
-			logger.debug("exportDone(): removing document fragment after MOVE action");
+			logger.debug("exportDone(): removing document fragment ({}, {}) after MOVE action",
+					selectionStart, selectionEnd);
 			document.remove(selectionStart, selectionEnd - selectionStart);
 			document.removeCleanup(selectionStart, selectionEnd);
 		} catch (BadLocationException e) {
 			logger.error("exportDone(): invalid document position", e);
+		} finally {
+			logger.trace("exportDone(source, {}, {}): leaving", data, action);
 		}
 	}
 }
