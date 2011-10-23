7fd985d5aeeed097f4916e181263a93a59a8cd24
Mykola Rybak <mykola.rybak@gmail.com>
2011-10-23 18:15:33 +0300

    Тест на загрузку пустого документа.

--- src/test/java/ru/socionicasys/analyst/LegacyHtmlReaderTest.java
+++ src/test/java/ru/socionicasys/analyst/LegacyHtmlReaderTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 import static org.testng.Assert.assertFalse;
@@ -12,12 +13,26 @@ import static org.testng.Assert.assertTrue;
 @SuppressWarnings({"OverlyBroadThrowsClause", "ThrowInsideCatchBlockWhichIgnoresCaughtException"})
 public class LegacyHtmlReaderTest {
 	@Test(expectedExceptions = FileNotFoundException.class)
-	public void testLoadInvalid() throws Throwable {
-		File tempFile = File.createTempFile("invalid", ".txt");
+	public void testLoadNonexistant() throws Throwable {
+		File tempFile = File.createTempFile("nonexistant", ".htm");
 		assertTrue(tempFile.delete());
 		assertFalse(tempFile.exists());
 		
 		LegacyHtmlReader reader = new LegacyHtmlReader(tempFile);
+		reader.execute();
+		try {
+			reader.get();
+		} catch (ExecutionException e) {
+			throw e.getCause();
+		}
+	}
+
+	@Test(expectedExceptions = StringIndexOutOfBoundsException.class)
+	public void testLoadEmpty() throws Throwable {
+		URL invalidFileUrl = getClass().getResource("LegacyHtmlReader/empty.htm");
+		File invalidFile = new File(invalidFileUrl.toURI());
+
+		LegacyHtmlReader reader = new LegacyHtmlReader(invalidFile);
 		reader.execute();
 		try {
 			reader.get();
